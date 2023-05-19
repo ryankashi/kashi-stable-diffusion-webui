954798e51223ecdfee6896f2234d9dc122cc8d29
ryankashi <ryankashi@berkeley.edu>
2023-05-19 08:50:36 -0700

    updated the .bat file

--- webui-user.bat
+++ webui-user.bat
@@ -1,8 +1,8 @@
 @echo off
 
-set PYTHON="C:\Program Files\Python310\python.exe"
+set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=--xformers --theme dark --autolaunch --api --no-half-vae --cors-allow-origins http://localhost:3000
+set COMMANDLINE_ARGS=--xformers --theme dark --api --no-half-vae --cors-allow-origins http://localhost:3000
 
 call webui.bat
