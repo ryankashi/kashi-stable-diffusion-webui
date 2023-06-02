f5a371a282460f47aacd936bc18efc4c7d23549c
ryankashi <ryankashi@berkeley.edu>
2023-06-01 17:25:22 -0700

    Removed xformers, added --precision full --no-half --skip-torch-cuda-test

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,6 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=--xformers --theme dark --api --autolaunch --nowebui --port 7860 --no-half-vae --cors-allow-origins http://localhost:3000
+set COMMANDLINE_ARGS=--precision full --no-half --skip-torch-cuda-test --theme dark --api --nowebui --port 7860 --no-half-vae --cors-allow-origins http://localhost:3000
 
 call webui.bat
