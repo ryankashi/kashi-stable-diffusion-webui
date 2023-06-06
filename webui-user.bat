6f63bb9541184c4cec59ce153029566347e087ab
ryankashi <ryankashi@berkeley.edu>
2023-06-05 17:58:36 -0700

    updated to enable localhost:3434

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,6 @@
 set PYTHON="C:\Program Files\Python310\python.exe"
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=--xformers --theme dark --autolaunch --api --no-half-vae --cors-allow-origins http://localhost:3000
+set COMMANDLINE_ARGS=--xformers --theme dark --autolaunch --api --no-half-vae --cors-allow-origins http://localhost:3434,http://localhost:3000
 
 call webui.bat
