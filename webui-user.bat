9e89fc74e42ce709b27a6c303ea5f7e830828b26
ryankashi <ryankashi@berkeley.edu>
2023-06-10 12:21:38 -0700

    removed the precision full --no-half stuff. Reenabled xformers

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,6 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=--precision full --no-half --skip-torch-cuda-test --theme dark --api --nowebui --port 7860 --no-half-vae --cors-allow-origins http://localhost:3434,http://localhost:3000
+set COMMANDLINE_ARGS=--xformers --skip-torch-cuda-test --theme dark --api --nowebui --port 7860 --no-half-vae --cors-allow-origins http://localhost:3434,http://localhost:3000
 
 call webui.bat
