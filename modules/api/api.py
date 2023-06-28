c554ee354a39084a7bb54cfe5b4b8cfc31843341
ryankashi <ryankashi@berkeley.edu>
2023-06-28 16:53:55 -0700

    testing stuff on kashi dev

--- modules/api/api.py
+++ modules/api/api.py
@@ -145,6 +145,7 @@ def api_middleware(app: FastAPI):
                 console.print_exception(show_locals=True, max_frames=2, extra_lines=1, suppress=[anyio, starlette], word_wrap=False, width=min([console.width, 200]))
             else:
                 errors.report(message, exc_info=True)
+            # raise HTTPException(status_code=500) from e
         return JSONResponse(status_code=vars(e).get('status_code', 500), content=jsonable_encoder(err))
 
     @app.middleware("http")
