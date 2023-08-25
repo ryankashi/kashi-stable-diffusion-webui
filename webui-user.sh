35416a0519ec8e9b49fc2f81e1237830ebc90bd2
ryankashi <ryanakashi@gmail.com>
2023-08-25 01:02:28 -0700

    Got our shit working. This is some huge news tbh

--- webui-user.sh
+++ webui-user.sh
@@ -9,19 +9,21 @@
 # Name of the subdirectory
 #clone_dir="stable-diffusion-webui"
 
-export COMMANDLINE_ARGS="--skip-torch-cuda-test --disable-console-progressbars --theme dark --api --port 7482 --no-half-vae --cors-allow-origins http://localhost:3434,http://localhost:3000 --ckpt-dir /home/ryan/Desktop/makeayo/desktop_diffusion/package_windows/kashi-stable-diffusion-webui-mastermodels/Stable-diffusion --lora-dir /home/ryan/Desktop/makeayo/desktop_diffusion/package_windows/kashi-stable-diffusion-webui-mastermodels/Lora --vae-dir /home/ryan/Desktop/makeayo/desktop_diffusion/package_windows/kashi-stable-diffusion-webui-mastermodels/VAE --opt-sub-quad-attention --disable-nan-check"
+export COMMANDLINE_ARGS="--skip-torch-cuda-test --disable-console-progressbars --theme dark --api --port 7482 --no-half-vae --cors-allow-origins http://localhost:3434,http://localhost:3000 --ckpt-dir '/home/ryan/Desktop/makeayo/desktop_diffusion/package_windows/kashi-stable-diffusion-webui-master/models/Stable-diffusion' --lora-dir '/home/ryan/Desktop/makeayo/desktop_diffusion/package_windows/kashi-stable-diffusion-webui-master/models/Lora' --vae-dir '/home/ryan/Desktop/makeayo/desktop_diffusion/package_windows/kashi-stable-diffusion-webui-master/models/VAE' --xformers"
+
+#export COMMANDLINE_ARGS=""
 
 # python3 executable
-python_cmd="python3"
+#python_cmd="python3"
 
 # git executable
-export GIT="git"
+#export GIT="git"
 
 # python3 venv without trailing slash (defaults to ${install_dir}/${clone_dir}/venv)
-venv_dir="venv"
+#venv_dir="venv"
 
 # script to launch to start the app
-export LAUNCH_SCRIPT="launch.py"
+#export LAUNCH_SCRIPT="launch.py"
 
 # install command for torch
 #export TORCH_COMMAND="pip install torch==1.12.1+cu113 --extra-index-url https://download.pytorch.org/whl/cu113"
