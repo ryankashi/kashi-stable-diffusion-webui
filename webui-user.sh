74713cdd26c38656c01eb90e9f8726c4e0425dd8
ryankashi <ryanakashi@gmail.com>
2023-08-24 23:33:51 -0700

    Fixed up the webui-user.sh. Ready to try cloning this to the package_windows directory

--- webui-user.sh
+++ webui-user.sh
@@ -9,20 +9,19 @@
 # Name of the subdirectory
 #clone_dir="stable-diffusion-webui"
 
-# Commandline arguments for webui.py, for example: export COMMANDLINE_ARGS="--medvram --opt-split-attention"
-#export COMMANDLINE_ARGS=""
+export COMMANDLINE_ARGS=""
 
 # python3 executable
-#python_cmd="python3"
+python_cmd="python3"
 
 # git executable
-#export GIT="git"
+export GIT="git"
 
 # python3 venv without trailing slash (defaults to ${install_dir}/${clone_dir}/venv)
-#venv_dir="venv"
+venv_dir="venv"
 
 # script to launch to start the app
-#export LAUNCH_SCRIPT="launch.py"
+export LAUNCH_SCRIPT="launch.py"
 
 # install command for torch
 #export TORCH_COMMAND="pip install torch==1.12.1+cu113 --extra-index-url https://download.pytorch.org/whl/cu113"
