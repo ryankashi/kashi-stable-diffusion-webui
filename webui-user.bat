cf1ab9c68bb13f2b630cc8fef880a4c0293f96db
ryankashi <ryankashi@berkeley.edu>
2023-09-28 22:02:09 -0700

    updated webui-user.bat and webui.bat

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,6 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
+set COMMANDLINE_ARGS=--theme dark --skip-torch-cuda-test --api --port 7482 --no-half-vae --backend directml --cors-allow-origins http://localhost:3434,http://localhost:3000
 
 call webui.bat
--- webui.bat
+++ webui.bat
@@ -3,7 +3,7 @@
 if not defined PYTHON (set PYTHON=python)
 if not defined VENV_DIR (set "VENV_DIR=%~dp0%venv")
 
-set SD_WEBUI_RESTART=tmp/restart
+
 set ERROR_REPORTING=FALSE
 
 mkdir tmp 2>NUL
@@ -31,7 +31,7 @@ if %ERRORLEVEL% == 0 goto :activate_venv
 
 for /f "delims=" %%i in ('CALL %PYTHON% -c "import sys; print(sys.executable)"') do set PYTHON_FULLNAME="%%i"
 echo Creating venv in directory %VENV_DIR% using python %PYTHON_FULLNAME%
-%PYTHON_FULLNAME% -m venv "%VENV_DIR%" >tmp/stdout.txt 2>tmp/stderr.txt
+%PYTHON_FULLNAME% -m virtualenv "%VENV_DIR%" >tmp/stdout.txt 2>tmp/stderr.txt
 if %ERRORLEVEL% == 0 goto :activate_venv
 echo Unable to create venv in directory "%VENV_DIR%"
 goto :show_stdout_stderr
@@ -51,14 +51,12 @@ if EXIST %ACCELERATE% goto :accelerate_launch
 
 :launch
 %PYTHON% launch.py %*
-if EXIST tmp/restart goto :skip_venv
 pause
 exit /b
 
 :accelerate_launch
 echo Accelerating
 %ACCELERATE% launch --num_cpu_threads_per_process=6 launch.py
-if EXIST tmp/restart goto :skip_venv
 pause
 exit /b
 
